f1b203df65b3d5aeaa17d0610b416ebf1bf7eac6
Tani-7 <ohmonesir@outlook.com>
2025-05-17 02:23:53 +0300

    outputing pdf

---
+++ SG_invoices/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import os
+from datetime import datetime
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    today = datetime.today().strftime("%B %-d, %Y")
+    return render_template('invoices.html', date=today)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
